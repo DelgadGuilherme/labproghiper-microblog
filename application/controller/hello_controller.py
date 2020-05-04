b25f209ce4b39e7fc67782ac50af9c2d9fd035a5
DelgadGuilherme <guilhermedms@hotmail.com>
2020-05-04 16:17:46 -0300

    Feat: controller

--- application/controller/hello_controller.py
+++ application/controller/hello_controller.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
+from flask import render_template, request
 from application import app
+from application.model.dao.usuario_dao import UsuarioDAO
+from application.model.entity.usuario import Usuario
+
+usuarioAutenticado = None
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return render_template('index.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+        usuario = Usuario(email, senha, None)
+        autenticacao = UsuarioDAO().login(usuario)
+
+        if autenticacao.is_sucesso():  # o python ja verifica se é True
+            usuarioAutenticado = autenticacao.get_usuario_autenticado()
+            return render_template('index.html')
+        elif autenticacao.is_nao_existe():
+            return render_template('register.html')
+        elif autenticacao.is_invalido():
+            return render_template('index.html', login_invalido=True, usuario=usuario)
+
+    if request.method == 'GET':
+        return render_template('index.html')
